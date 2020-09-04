dea93fd41b4abda8b0b3e71e37da003bf103594f
KevinTorresRivera <kevin.torres33@upr.edu>
2020-09-03 21:59:40 -0400

    Calling out Pow

---
+++ Hocus_pocus.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    double number1;
+    int number2;
+
+    cout << "Enter the first number " <<endl;
+    cin >> number1;
+
+    cout << "Enter the second number" <<endl;
+    cin>> number2;
+
+
+    return 0;
+}
